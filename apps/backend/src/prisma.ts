d0854cec5c374d2276dd519e3ed4de28481942f9
MayankG024 <mayankynr24@gmail.com>
2026-06-04 19:22:02 +0530

    added pg pool error listener and pgbouncer query parameter

--- apps/backend/src/prisma.ts
+++ apps/backend/src/prisma.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 import pg from 'pg';
 import { env } from './config/env.js';
+import { logger } from './utils/logger.js';
 
 declare global {
   // eslint-disable-next-line no-var
@@ -9,6 +10,9 @@ declare global {
 }
 
 const pool = new pg.Pool({ connectionString: env.DATABASE_URL });
+pool.on('error', (err) => {
+  logger.error(err, 'Unexpected error on idle PostgreSQL client pool');
+});
 const adapter = new PrismaPg(pool);
 
 export const prisma = globalThis.prisma || new PrismaClient({
